9bfc015f8c8f66c76364eaf94575fd061b80fe03
mobilemancer <awt@ProdFrog.localdomain>
2020-11-01 21:54:03 +0100

    api

--- api/GetProducts.cs
+++ api/GetProducts.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -6,30 +5,20 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace ESMUG.Demo
 {
   public static class GetProducts
   {
-    [FunctionName("GetProducts")]
+    [FunctionName(nameof(GetProducts))]
     public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        ExecutionContext context,
         ILogger log)
     {
-      log.LogInformation("C# HTTP trigger function processed a request.");
-
-      string name = req.Query["name"];
-
-      string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-      dynamic data = JsonConvert.DeserializeObject(requestBody);
-      name = name ?? data?.name;
-
-      string responseMessage = string.IsNullOrEmpty(name)
-          ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.\n"
-          : $"Hello, {name}. This HTTP triggered function executed successfully.\n";
-
-      return new OkObjectResult(responseMessage);
+      log.LogInformation($"{nameof(GetProducts)} called.");
+      var products = await File.ReadAllTextAsync(Path.Combine(context.FunctionAppDirectory, "data", "products.json"));
+      return new OkObjectResult(products);
     }
   }
 }
